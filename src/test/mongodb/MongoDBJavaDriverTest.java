a929467311baa1d555981be37bf9a1e033b0c2fe
chiewenscot <chiewen.scot@gmail.com>
2016-03-01 00:54:13 -0500

    add more scenario

--- src/test/mongodb/MongoDBJavaDriverTest.java
+++ src/test/mongodb/MongoDBJavaDriverTest.java
@@ -30,6 +30,46 @@ import com.mongodb.client.result.UpdateResult;
  * 
  * sample data for importing into MongoDB:
  * dataset.json in the same folder (file downloaded from MongoDB web-site)
+ * 
+ * #### Sample restaurants data: ####
+ * {
+ *	"_id" : ObjectId("56d34579654866c5a7adfb7a"),
+ *	"address" : {
+ *		"building" : "351",
+ *		"coord" : [
+ *			-73.98513559999999,
+ *			40.7676919
+ *		],
+ *		"street" : "West   57 Street",
+ *		"zipcode" : "10019"
+ *	},
+ *	"borough" : "Manhattan",
+ *	"cuisine" : "Irish",
+ *	"grades" : [
+ *		{
+ *			"date" : ISODate("2014-09-06T00:00:00.000Z"),
+ *			"grade" : "A",
+ *			"score" : 2
+ *		},
+ *		{
+ *			"date" : ISODate("2013-07-22T00:00:00.000Z"),
+ *			"grade" : "A",
+ *			"score" : 11
+ *		},
+ *		{
+ *			"date" : ISODate("2012-07-31T00:00:00.000Z"),
+ *			"grade" : "A",
+ *			"score" : 12
+ *		},
+ *		{
+ *			"date" : ISODate("2011-12-29T00:00:00.000Z"),
+ *			"grade" : "A",
+ *			"score" : 12
+ *		}
+ *	],
+ *	"name" : "Dj Reynolds Pub And Restaurant",
+ *	"restaurant_id" : "30191841"
+ *}
  * **/
 public class MongoDBJavaDriverTest {
 
